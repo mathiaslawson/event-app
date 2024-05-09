582955ddd1468009deef731a6f59e2987cf117cc
Hancho7 <litorojose1@gmail.com>
2024-05-09 17:16:26 +0000

    webhooks

--- server/controllers/webhook.js
+++ server/controllers/webhook.js
@@ -2,13 +2,21 @@ const crypto = require("crypto");
 
 const webhook = async (req, res) => {
   const secret = process.env.PAYSTACK_SECRET_KEY;
+  
+  // Create hash from request body
   const hash = crypto
     .createHmac("sha512", secret)
     .update(JSON.stringify(req.body))
     .digest("hex");
-  if (hash == req.headers["x-paystack-signature"]) {
+
+  // Verify the hash with the signature in request headers
+  if (hash === req.headers["x-paystack-signature"]) {
     const event = req.body;
+    console.log("Request body:", req.body); // Original request body
+    console.log("Hash:", hash); // Hash of the request body
+  } else {
+    console.log("Hash mismatch. Request might be tampered with.");
   }
-  console.log("webhooks", hash);
 };
+
 module.exports = { webhook };
